e3c6f2663de184d22ba76fd25d907ebe4302df7e
Braydon <braydonru1234@gmail.com>
2026-03-01 16:21:11 -0500

    Human Resources

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -9,7 +9,13 @@ const Footer = () => {
                 <div className="row px-xl-5 pt-5" >
                     <div className="col-lg-5 col-md-12 mb-5 pr-3 pr-xl-5">
                         <Link to="" className="text-decoration-none">
-                            <h1 className="mb-4 display-5 font-weight-semi-bold" style={{color: "#EFB810"}}>Golden Sunset Made by Josh</h1>
+                            <h1 className="mb-4 display-5 font-weight-semi-bold" style={{
+                                background: 'linear-gradient(135deg, #CD7F32, #DAA520)',
+                                color: 'transparent',
+                                WebkitBackgroundClip: 'text',
+                                backgroundClip: 'text',
+                                display: 'inline-block'
+                            }}>Golden Sunset Made by Josh</h1>
                         </Link>
                         <p className="mb-2"><i className="fa fa-map-marker-alt text-primary mr-3"></i>2132 Monterrey Blvd, Hermosa Beach, CA 90254</p>
                         <p className="mb-2"><i className="fa fa-envelope text-primary mr-3"></i>goldensunsetjosh@gmail.com</p>
